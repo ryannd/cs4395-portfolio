c4e59e3a3c7448eae39a37a6066dae808757e7fe
ryannd <48656699+ryannd@users.noreply.github.com>
2022-08-30 05:04:12 +0000

    Deploying to gh-pages from @ ryannd/cs4395-portfolio@6d8a3930c1c1c7472343fdeecae9470c393b85e1 🚀

---
+++ _next/static/Xd-hzh1GViC0agwNNGqMB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ff388c938ff6fc6b.js"],"/_error":["static/chunks/pages/_error-cb181bc066ffb5d9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/pages/index-ff388c938ff6fc6b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5557:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return c(3678)}])},3678:function(a,b,c){"use strict";c.r(b);var d=c(5893);b.default=function(){return(0,d.jsx)("div",{children:"Welcome to Next.js!"})}}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=5557)}),_N_E=a.O()}])
