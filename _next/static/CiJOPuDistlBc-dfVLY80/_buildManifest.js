3b04ceb3e598cde12f1f15d853c04c9f89d852a3
ryannd <48656699+ryannd@users.noreply.github.com>
2022-08-30 21:19:28 +0000

    Deploying to gh-pages from @ ryannd/cs4395-portfolio@1e5aee298c3d18b100a70e21766ef2742b7cd8a0 🚀

--- _next/static/CiJOPuDistlBc-dfVLY80/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ff388c938ff6fc6b.js"],"/_error":["static/chunks/pages/_error-cb181bc066ffb5d9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
